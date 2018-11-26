0f6fdd6974772d9170935296156793c2aa2ccd5a
Clemens Akens <clebert@users.noreply.github.com>
2018-11-26 13:26:30 +0100

    feat(all): merge all React packages, closes #30 (#31)

---
+++ packages/react/src/__tests__/feature-app-loader.node.test.tsx
@@ -0,0 +1,69 @@
+/**
+ * @jest-environment node
+ */
+
+import {
+  AsyncValue,
+  FeatureAppDefinition,
+  FeatureAppManagerLike
+} from '@feature-hub/core';
+import {shallow} from 'enzyme';
+import * as React from 'react';
+import {FeatureAppLoader} from '..';
+
+describe('FeatureAppLoader (on Node.js)', () => {
+  let mockManager: FeatureAppManagerLike;
+  let mockGetAsyncFeatureAppDefinition: jest.Mock;
+  let mockAsyncFeatureAppDefinition: AsyncValue<FeatureAppDefinition<unknown>>;
+  let spyConsoleError: jest.SpyInstance;
+
+  beforeEach(() => {
+    mockAsyncFeatureAppDefinition = new AsyncValue(new Promise(jest.fn()));
+
+    mockGetAsyncFeatureAppDefinition = jest.fn(
+      () => mockAsyncFeatureAppDefinition
+    );
+
+    mockManager = {
+      getAsyncFeatureAppDefinition: mockGetAsyncFeatureAppDefinition,
+      getFeatureAppScope: jest.fn(),
+      preloadFeatureApp: jest.fn(),
+      destroy: jest.fn()
+    };
+
+    spyConsoleError = jest.spyOn(console, 'error');
+    spyConsoleError.mockImplementation(jest.fn());
+  });
+
+  afterEach(() => {
+    spyConsoleError.mockRestore();
+  });
+
+  describe('when the async feature app definition synchronously has an error', () => {
+    let mockError: Error;
+
+    beforeEach(() => {
+      mockError = new Error('Failed to load feature app module.');
+      mockAsyncFeatureAppDefinition.error = mockError;
+    });
+
+    it('logs and re-throws the error', () => {
+      expect(() =>
+        shallow(
+          <FeatureAppLoader
+            manager={mockManager}
+            src="example.js"
+            featureAppKey="testKey"
+          />
+        )
+      ).toThrowError(mockError);
+
+      expect(spyConsoleError.mock.calls).toEqual([
+        [
+          'The feature app for the url "example.js" and the key "testKey" could not be loaded.',
+          mockError
+        ]
+      ]);
+    });
+  });
+});
---
+++ packages/react/src/internal/type-guards.ts
@@ -0,0 +1,27 @@
+// tslint:disable:strict-type-predicates
+
+import {
+  DomFeatureApp,
+  FeatureApp,
+  ReactFeatureApp
+} from '../feature-app-container';
+
+export function isDomFeatureApp(featureApp: {
+  [key: string]: unknown;
+}): featureApp is DomFeatureApp {
+  return typeof featureApp.attachTo === 'function';
+}
+
+export function isReactFeatureApp(featureApp: {
+  [key: string]: unknown;
+}): featureApp is ReactFeatureApp {
+  return typeof featureApp.render === 'function';
+}
+
+export function isFeatureApp(featureApp: unknown): featureApp is FeatureApp {
+  if (typeof featureApp !== 'object' || !featureApp) {
+    return false;
+  }
+
+  return isDomFeatureApp(featureApp) || isReactFeatureApp(featureApp);
+}
