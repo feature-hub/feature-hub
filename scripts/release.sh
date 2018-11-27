81d8ccd7a7af5de90d2d5dc85e6459d8173f80c8
Hendrik Liebau <mail@hendrik-liebau.de>
2018-11-27 19:05:27 +0100

    ci(all): use custom config for custom build triggers (#62)

--- scripts/release.sh
+++ scripts/release.sh
@@ -7,9 +7,9 @@ echo "//registry.npmjs.org/:_authToken=\${NPM_TOKEN}" > .npmrc
 git config --global user.email "feature-hub@sinnerschrader.com"
 git config --global user.name "Feature Hub CI"
 
-git remote update
-git fetch
-git checkout -b master --track origin/master
+git remote add writable-origin "https://${GITHUB_TOKEN}@github.com/sinnerschrader/feature-hub.git"
+git fetch writable-origin
+git checkout -b master --track writable-origin/master
 
-npx lerna version --conventional-commits --yes --git-remote "https://${GITHUB_TOKEN}@github.com/sinnerschrader/feature-hub.git" --no-push
-# npx lerna publish from-git --yes
+npx lerna version --conventional-commits --yes --git-remote writable-origin
+npx lerna publish from-git --yes
