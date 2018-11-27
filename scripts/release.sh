621830566724162137a93e80515428a6a9091ea2
Clemens Akens <clebert@me.com>
2018-11-27 22:28:57 +0100

    ci(all): add missing GitHub username to release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -7,7 +7,7 @@ echo "//registry.npmjs.org/:_authToken=\${NPM_TOKEN}" > .npmrc
 git config --global user.email "feature-hub@sinnerschrader.com"
 git config --global user.name "Feature Hub CI"
 
-git remote add writable-origin "https://${GITHUB_TOKEN}@github.com/sinnerschrader/feature-hub.git"
+git remote add writable-origin "https://feature-hub-ci:${GITHUB_TOKEN}@github.com/sinnerschrader/feature-hub.git"
 git checkout master
 
 npx lerna version --conventional-commits --yes --git-remote writable-origin
