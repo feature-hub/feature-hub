c5b496e0f7224f58050446f9b5f2e00c5dea1f8c
Hendrik Liebau <mail@hendrik-liebau.de>
2018-11-28 00:20:03 +0100

    ci(all): use npm as publish client

--- scripts/release.sh
+++ scripts/release.sh
@@ -10,4 +10,4 @@ git config --global user.name "Feature Hub CI"
 git checkout master
 
 npx lerna version --conventional-commits --yes
-npx lerna publish from-git --yes
+npx lerna publish from-git --npm-client npm --yes
