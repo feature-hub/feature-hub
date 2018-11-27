9cc235254f46cbb03cc325871ce3dcd43884a67a
Hendrik Liebau <mail@hendrik-liebau.de>
2018-11-28 00:02:35 +0100

    ci(all): setup npm release (#68)

---
+++ scripts/release.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e
+
+echo "//registry.npmjs.org/:_authToken=\${NPM_TOKEN}" > .npmrc
+
+git config --global user.email "feature-hub@sinnerschrader.com"
+git config --global user.name "Feature Hub CI"
+
+git checkout master
+
+npx lerna version --conventional-commits --yes
+npx lerna publish from-git --yes
