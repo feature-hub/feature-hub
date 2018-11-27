4e80f348cc08c109e02b359502807afd1b23f064
Hendrik Liebau <mail@hendrik-liebau.de>
2018-11-27 19:32:13 +0100

    ci(all): do not checkout master before publish (#64)

--- scripts/release.sh
+++ scripts/release.sh
@@ -8,8 +8,6 @@ git config --global user.email "feature-hub@sinnerschrader.com"
 git config --global user.name "Feature Hub CI"
 
 git remote add writable-origin "https://${GITHUB_TOKEN}@github.com/sinnerschrader/feature-hub.git"
-git fetch writable-origin
-git checkout -b master --track writable-origin/master
 
 npx lerna version --conventional-commits --yes --git-remote writable-origin
 npx lerna publish from-git --yes
