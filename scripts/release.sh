e8663bb2e86f74b51c229e81e1e4144b65e7a6be
Hendrik Liebau <mail@hendrik-liebau.de>
2018-11-27 15:59:31 +0100

    ci(all): test release script (no push, no publish) (#55)

--- scripts/release.sh
+++ scripts/release.sh
@@ -7,7 +7,6 @@ echo "//registry.npmjs.org/:_authToken=\${NPM_TOKEN}" > .npmrc
 git config --global user.email "feature-hub@sinnerschrader.com"
 git config --global user.name "Feature Hub CI"
 
-git reset --hard HEAD~1
-npx lerna version --conventional-commits --yes
-git push "https://${GITHUB_TOKEN}@github.com/sinnerschrader/feature-hub.git" HEAD:master --follow-tags
-npx lerna publish from-git --yes
+git checkout master
+npx lerna version --conventional-commits --yes --git-remote "https://${GITHUB_TOKEN}@github.com/sinnerschrader/feature-hub.git" --no-push
+# npx lerna publish from-git --yes
