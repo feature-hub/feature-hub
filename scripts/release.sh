ac39782caf9a87827a6d2a266c656bc5ef0d7117
Hendrik Liebau <mail@hendrik-liebau.de>
2018-11-27 16:29:47 +0100

    ci(all): fetch before checking out master (#57)

--- scripts/release.sh
+++ scripts/release.sh
@@ -7,6 +7,9 @@ echo "//registry.npmjs.org/:_authToken=\${NPM_TOKEN}" > .npmrc
 git config --global user.email "feature-hub@sinnerschrader.com"
 git config --global user.name "Feature Hub CI"
 
-git checkout master
+git remote update
+git fetch
+git checkout -b master --track origin/master
+
 npx lerna version --conventional-commits --yes --git-remote "https://${GITHUB_TOKEN}@github.com/sinnerschrader/feature-hub.git" --no-push
 # npx lerna publish from-git --yes
