0d1e53917a94097edf76aba845fd152c1fadc021
Markus Wolf <KnisterPeter@users.noreply.github.com>
2018-11-28 12:16:45 +0100

    ci(all): add a blacklist script to prevent commits with disallowed words (#72)

---
+++ scripts/word-blacklist.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+if [ -z "$BLACKLIST" ] ; then
+  echo "Empty word blacklist; aborting"
+  exit 0
+fi
+
+# replace every comma with a pipe
+BLACKLIST_REGEXP=$(echo $BLACKLIST |tr "," "|")
+# read .gitignore, replace newlines with pipes and remove the last pipe
+IGNORE=$(cat .gitignore |tr "\n" "|" |sed -e 's/|$//')
+
+MATCHES=$(grep --extended-regexp --word-regexp --recursive --regexp="$BLACKLIST_REGEXP" . |grep --extended-regexp --invert-match --regexp="$IGNORE" |grep --extended-regexp --invert-match --regexp=".git|docs/api")
+RESULT=$?
+
+if [ "$CI" != "true" ] ; then
+  echo $MATCHES
+fi
+
+if [ $RESULT -eq 0 ] ; then
+  echo "Found blacklisted word in sources. Failing..."
+  exit 1
+else
+  exit 0
+fi
