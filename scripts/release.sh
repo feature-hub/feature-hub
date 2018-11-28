07ead33cfd464968595dd84ccde413c65470baa1
Hendrik Liebau <mail@hendrik-liebau.de>
2018-11-28 09:57:06 +0100

    ci(all): move git and npm config to before_script of travis.yml

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,11 +2,6 @@
 
 set -e
 
-echo "//registry.npmjs.org/:_authToken=\${NPM_TOKEN}" > .npmrc
-
-git config --global user.email "feature-hub@sinnerschrader.com"
-git config --global user.name "Feature Hub CI"
-
 git checkout master
 
 npx lerna version --conventional-commits --yes
