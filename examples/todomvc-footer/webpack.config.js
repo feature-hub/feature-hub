b990806cb8ce2d549889d08c83f7e90d5878d4bd
Hendrik Liebau <mail@hendrik-liebau.de>
2018-11-30 22:05:00 +0100

    build(examples): switch TodoMVC footer to webpack

---
+++ examples/todomvc-footer/webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    filename: 'todomvc-footer.js',
+    path: path.resolve(__dirname, 'dist'),
+    libraryTarget: 'umd'
+  },
+  resolve: {
+    extensions: ['.js', '.json', '.ts', '.tsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: {
+          loader: 'ts-loader',
+          options: {
+            configFile: path.resolve(__dirname, 'tsconfig.build.json')
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              modules: true,
+              localIdentName: '[path][name]__[local]--[hash:base64:5]',
+              sourceMap: true
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              plugins: [require('postcss-cssnext')]
+            }
+          }
+        ]
+      }
+    ]
+  },
+  devtool: 'source-map',
+  externals: {
+    react: 'react'
+  }
+};
