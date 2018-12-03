05f1b29ffaf05361276b24df144f54c70071edc2
Hendrik Liebau <mail@hendrik-liebau.de>
2018-12-03 17:07:34 +0100

    ci(all): only push to git when publish succeeded

--- scripts/ci/release.sh
+++ scripts/ci/release.sh
@@ -4,5 +4,7 @@ set -e
 
 git checkout master
 
-npx lerna version --conventional-commits --yes
+npx lerna version --conventional-commits --no-push --yes
 npx lerna publish from-git --npm-client npm --yes
+
+git push --follow-tags
