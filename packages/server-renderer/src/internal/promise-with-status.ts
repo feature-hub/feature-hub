2817e482e9fcf79c526b3e5c63e64cd82c37f89f
Hendrik Liebau <mail@hendrik-liebau.de>
2018-12-05 11:56:58 +0100

    fix(server-renderer): make PromiseWithStatus#then async

--- packages/server-renderer/src/internal/promise-with-status.ts
+++ packages/server-renderer/src/internal/promise-with-status.ts
@@ -17,7 +17,7 @@ export class PromiseWithStatus<TResult> {
     return this._status;
   }
 
-  public then<TResult1 = TResult, TResult2 = never>(
+  public async then<TResult1 = TResult, TResult2 = never>(
     onfulfilled?: Onfulfilled<TResult, TResult1> | undefined | null,
     onrejected?: Onrejected<TResult2> | undefined | null
   ): Promise<TResult1 | TResult2> {
