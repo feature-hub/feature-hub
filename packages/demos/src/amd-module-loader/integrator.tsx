219e4802f5875d3d0b2419d770bf6fc59ac7fe7a
Clemens Akens <clebert@me.com>
2019-01-02 16:07:12 +0100

    refactor(demos): improve imports

--- packages/demos/src/amd-module-loader/integrator.tsx
+++ packages/demos/src/amd-module-loader/integrator.tsx
@@ -3,7 +3,6 @@ import {defineExternals, loadAmdModule} from '@feature-hub/module-loader';
 import {FeatureAppLoader} from '@feature-hub/react';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-
 import '../blueprint-css';
 
 const registry = new FeatureServiceRegistry();
