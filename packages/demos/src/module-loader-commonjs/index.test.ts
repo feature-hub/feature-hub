312ecc94e299ea7116c064cb2e3270f7ca9b7b9e
Hendrik Liebau <mail@hendrik-liebau.de>
2019-01-28 16:50:27 +0100

    test(all): honor timeout in Browser#goto

--- packages/demos/src/module-loader-commonjs/index.test.ts
+++ packages/demos/src/module-loader-commonjs/index.test.ts
@@ -9,7 +9,7 @@ import {startServer} from '../start-server';
 import renderApp from './integrator.node';
 import webpackConfigs from './webpack-config';
 
-jest.setTimeout(120000);
+jest.setTimeout(60000);
 
 describe('integration test: "commonjs module loader"', () => {
   const browser = new Browser(5000);
@@ -21,7 +21,7 @@ describe('integration test: "commonjs module loader"', () => {
 
     const {port} = server.address() as AddressInfo;
 
-    await browser.goto(`http://localhost:${port}`, 120000);
+    await browser.goto(`http://localhost:${port}`, 60000);
   });
 
   afterAll(done => server.close(done));
