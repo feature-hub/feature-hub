0156cb352356cdcdf63b7b53416bc533b5a84bd4
Clemens Akens <clebert@me.com>
2019-02-22 14:40:10 +0100

    build(all): enable tslint rule no-submodule-imports

--- packages/demos/src/blueprint-css.ts
+++ packages/demos/src/blueprint-css.ts
@@ -1,4 +1,4 @@
-// tslint:disable:no-implicit-dependencies
+// tslint:disable:no-implicit-dependencies no-submodule-imports
 
 import '@blueprintjs/core/lib/css/blueprint.css';
 import '@blueprintjs/icons/lib/css/blueprint-icons.css';
