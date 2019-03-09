9bf966737ccc72e9e9da2e963819c574ad0ffc21
Clemens Akens <clebert@me.com>
2019-03-10 00:00:52 +0100

    feat(rcgen): improve format module

--- packages/rcgen/src/format.ts
+++ packages/rcgen/src/format.ts
@@ -3,22 +3,31 @@ import {mergePrettierConfig} from './prettier';
 import {mergeVscodeSettings} from './vscode';
 
 export interface FormatOptions {
-  readonly onSave?: boolean;
-  readonly useTabs?: boolean;
-  readonly tabWidth?: number;
+  readonly formatOnSave?: boolean;
+  readonly indentStyle?: 'tab' | 'space';
+  readonly indentSize?: number;
   readonly endOfLine?: 'lf' | 'cr' | 'crlf';
 }
 
 export function mergeFormat(options: FormatOptions = {}): ManifestEnhancer {
   const {
-    onSave = true,
-    useTabs = false,
-    tabWidth = 2,
+    formatOnSave = true,
+    indentStyle = 'space',
+    indentSize = 2,
     endOfLine = 'lf'
   } = options;
 
   return enhanceManifest(
-    mergePrettierConfig({useTabs, tabWidth, endOfLine}),
-    mergeVscodeSettings({'editor.formatOnSave': onSave})
+    mergePrettierConfig({
+      useTabs:
+        indentStyle === 'tab'
+          ? true
+          : indentStyle === 'space'
+          ? false
+          : undefined,
+      tabWidth: indentSize,
+      endOfLine
+    }),
+    mergeVscodeSettings({'editor.formatOnSave': formatOnSave})
   );
 }
