a682126ffadeb035f563b257f335a16014df7967
Hendrik Liebau <mail@hendrik-liebau.de>
2019-03-19 11:28:16 +0100

    build(all): use yarn in netlify script

--- scripts/netlify/build-website.sh
+++ scripts/netlify/build-website.sh
@@ -2,8 +2,7 @@
 
 set -e
 
-$(npm bin)/lerna exec --scope @feature-hub/website -- rm -rf build
-$(npm bin)/lerna exec --scope @feature-hub/website -- docusaurus-build
-$(npm bin)/typedoc --options typedoc.js .
-node scripts/process-api-docs.js
-$(npm bin)/lerna run --scope @feature-hub/demos build:todomvc
+yarn lerna exec --scope @feature-hub/website 'rm -rf build'
+yarn lerna exec --scope @feature-hub/website 'docusaurus-build'
+yarn run generate:api-docs
+yarn lerna run --scope @feature-hub/demos build:todomvc
