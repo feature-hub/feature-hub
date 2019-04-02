881bb9d76cd4ec8055eb44aad3b40ef7d979216e
Hendrik Liebau <mail@hendrik-liebau.de>
2019-04-02 09:49:11 +0200

    ci(all): remove travis matrix again

--- scripts/ci/test-travis.sh
+++ scripts/ci/test-travis.sh
@@ -2,16 +2,12 @@
 
 set -e
 
-if [ "$TEST_SUITE" = "unit" ]
-then
-  ./scripts/ci/word-blacklist.sh
+./scripts/ci/word-blacklist.sh
 
-  yarn commitlint-travis
+yarn commitlint-travis
 
-  yarn test:unit --no-cache --maxWorkers 2 --no-verbose
-  yarn lint
-  yarn compile
-  yarn verify
-else
-  node --max_old_space_size=4096 $(npm bin)/jest --no-cache --maxWorkers 2 --no-verbose --no-coverage --logHeapUsage --testPathPattern packages/demos
-fi
+yarn test:unit --no-cache --maxWorkers 2 --no-verbose
+node --max_old_space_size=4096 $(npm bin)/jest --no-cache --maxWorkers 2 --no-verbose --no-coverage --logHeapUsage --testPathPattern packages/demos
+yarn lint
+yarn compile
+yarn verify
