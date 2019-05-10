4b36220e9035853bba936371c9c0ef614168ae41
Hendrik Liebau <mail@hendrik-liebau.de>
2019-05-10 15:48:37 +0200

    test(demos): add integration test for custom logging & beforeCreate callback

--- packages/demos/src/custom-logging/app.tsx
+++ packages/demos/src/custom-logging/app.tsx
@@ -1,17 +1,27 @@
+import {FeatureServices} from '@feature-hub/core';
 import {FeatureAppContainer} from '@feature-hub/react';
 import * as React from 'react';
 import featureAppDefinition from './feature-app';
 
-export function App(): JSX.Element {
+export interface AppProps {
+  readonly beforeCreate?: (
+    consumerUid: string,
+    featureServices: FeatureServices
+  ) => void;
+}
+
+export function App({beforeCreate}: AppProps): JSX.Element {
   return (
     <>
       <FeatureAppContainer
         featureAppDefinition={featureAppDefinition}
         idSpecifier="first"
+        beforeCreate={beforeCreate}
       />
       <FeatureAppContainer
         featureAppDefinition={featureAppDefinition}
         idSpecifier="second"
+        beforeCreate={beforeCreate}
       />
     </>
   );
---
+++ packages/demos/src/custom-logging/index.test.ts
@@ -0,0 +1,57 @@
+/**
+ * @jest-environment puppeteer
+ */
+
+import {Server} from 'http';
+import {AddressInfo} from 'net';
+import {ConsoleMessage} from 'puppeteer';
+import {Browser} from '../browser';
+import {startServer} from '../start-server';
+import {stubConsole} from '../stub-console';
+import webpackConfigs from './webpack-config';
+
+jest.setTimeout(120000);
+
+stubConsole();
+
+describe('integration test: "custom logging"', () => {
+  const browser = new Browser(5000);
+
+  let server: Server;
+  let consoleMessages: ConsoleMessage[];
+
+  beforeAll(async () => {
+    server = await startServer(webpackConfigs, undefined);
+    consoleMessages = [];
+
+    page.on('console', consoleMessages.push.bind(consoleMessages));
+
+    const {port} = server.address() as AddressInfo;
+
+    await browser.goto(`http://localhost:${port}`, 120000);
+  });
+
+  afterAll(done => server.close(done));
+
+  it('logs with the custom logger', async () => {
+    const messages = await Promise.all(
+      consoleMessages.map(async consoleMessage =>
+        Promise.all(
+          consoleMessage.args().map(async handle => handle.jsonValue())
+        )
+      )
+    );
+
+    expect(messages).toContainEqual([
+      '%ctest:integrator',
+      'font-weight: bold',
+      'Creating Feature App with consumerUid "test:logging-app:first"...'
+    ]);
+
+    expect(messages).toContainEqual([
+      '%ctest:integrator',
+      'font-weight: bold',
+      'Creating Feature App with consumerUid "test:logging-app:second"...'
+    ]);
+  });
+});
