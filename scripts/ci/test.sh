17cdc0dc2ce60c40a05350e2c814e922493cfe77
Hendrik Liebau <mail@hendrik-liebau.de>
2019-07-05 21:02:37 +0200

    test(all): silence test output for the ci as well

--- scripts/ci/test.sh
+++ scripts/ci/test.sh
@@ -3,7 +3,7 @@
 set -e
 
 yarn test:unit --no-cache --maxWorkers 2 --no-verbose
-find packages/demos -iname '*test.ts*' | xargs -n 2 yarn jest --no-cache --no-verbose --no-coverage --maxWorkers 2 --forceExit
+find packages/demos -iname '*test.ts*' | xargs -n 2 yarn jest --silent --no-cache --no-verbose --no-coverage --maxWorkers 2 --forceExit
 yarn lint
 yarn compile
 yarn verify
