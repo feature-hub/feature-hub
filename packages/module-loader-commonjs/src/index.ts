5be4e180fd9d0c9034978fd14a9e5309a149ea3d
Hendrik Liebau <mail@hendrik-liebau.de>
2019-07-30 08:35:20 +0200

    fix(module-loader-commonjs): avoid using fs to require commonjs module (#533)
    fixes #503

--- packages/module-loader-commonjs/src/index.ts
+++ packages/module-loader-commonjs/src/index.ts
@@ -1,42 +1,32 @@
 import {ModuleLoader} from '@feature-hub/core';
-import crypto from 'crypto';
-import fs from 'fs-extra';
 import fetch from 'node-fetch';
-import path from 'path';
 
-// This must be a local directory to make node module resolution possible.
-const tmpDirname = path.join(__dirname, '.tmp');
-
-function createHash(data: string): string {
-  return crypto
-    .createHash('md5')
-    .update(data)
-    .digest('hex');
+export interface Externals {
+  readonly [externalName: string]: unknown;
 }
 
-async function writeFile(url: string, data: Buffer): Promise<string> {
-  /* istanbul ignore next */
-  if (!(await fs.pathExists(tmpDirname))) {
-    await fs.mkdirp(tmpDirname);
-  }
+export function createCommonJsModuleLoader(
+  externals: Externals = {}
+): ModuleLoader {
+  return async (url: string): Promise<unknown> => {
+    const response = await fetch(url);
+    const source = await response.text();
+    const mod = {exports: {}};
 
-  const filename = path.join(tmpDirname, `${createHash(url)}.js`);
+    // tslint:disable-next-line:function-constructor
+    Function(
+      'module',
+      'exports',
+      'require',
+      `${source}
+      //# sourceURL=${url}`
+    )(mod, mod.exports, (dep: string) =>
+      // tslint:disable-next-line:no-eval https://stackoverflow.com/a/41063795/10385541
+      externals.hasOwnProperty(dep) ? externals[dep] : eval('require')(dep)
+    );
 
-  await fs.writeFile(filename, data);
-
-  return filename;
+    return mod.exports;
+  };
 }
 
-async function requireAsync(url: string): Promise<unknown> {
-  const response = await fetch(url);
-  const data = await response.buffer();
-  const filename = await writeFile(url, data);
-
-  // Make sure no stale version of the module is loaded from the require cache!
-  // tslint:disable-next-line:no-dynamic-delete
-  delete require.cache[require.resolve(filename)];
-
-  return require(filename);
-}
-
-export const loadCommonJsModule: ModuleLoader = async url => requireAsync(url);
+export const loadCommonJsModule: ModuleLoader = createCommonJsModuleLoader();
