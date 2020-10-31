5e654c726171250be311a5db5c4f7e1ce64c096f
Hendrik Liebau <mail@hendrik-liebau.de>
2020-10-31 12:41:55 +0100

    ci(all): remove azure and travis

--- scripts/ci/test-travis.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-set -e
-
-./scripts/ci/word-blacklist.sh
-yarn commitlint-travis
-./scripts/ci/test.sh
--- scripts/ci/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -e
-
-yarn test:unit --no-cache --maxWorkers 2 --no-verbose
-find packages/demos -iname '*test.ts*' | xargs -n 2 yarn jest --silent --no-cache --no-verbose --no-coverage --maxWorkers 2 --forceExit
-yarn lint
-yarn compile
-yarn verify
