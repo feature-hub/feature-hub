7d914b8e2da59d8d28147687a7b7089ef80aea17
Hendrik Liebau <mail@hendrik-liebau.de>
2021-06-23 09:49:44 +0200

    fix(demos): use blueprintjs in federation demo

--- packages/demos/src/module-loader-federation/feature-app.tsx
+++ packages/demos/src/module-loader-federation/feature-app.tsx
@@ -1,17 +1,14 @@
+import {Card, Text} from '@blueprintjs/core';
 import {FeatureAppDefinition} from '@feature-hub/core';
 import {ReactFeatureApp} from '@feature-hub/react';
 import * as React from 'react';
 
 const featureAppDefinition: FeatureAppDefinition<ReactFeatureApp> = {
-  dependencies: {
-    externals: {},
-  },
-
   create: () => ({
     render: () => (
-      <h3 style={{margin: '20px'}}>
-        <span>Hello, World!</span>
-      </h3>
+      <Card style={{margin: '20px'}}>
+        <Text>Hello, World!</Text>
+      </Card>
     ),
   }),
 };
