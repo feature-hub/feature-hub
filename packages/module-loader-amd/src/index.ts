c4edc8db7550265d456d635429b5fe27a06605ec
Hendrik Liebau <mail@hendrik-liebau.de>
2021-07-06 17:21:47 +0200

    fix(module-loader-amd): remove unused `moduleType` from signature

--- packages/module-loader-amd/src/index.ts
+++ packages/module-loader-amd/src/index.ts
@@ -1,10 +1,14 @@
-import {ModuleLoader} from '@feature-hub/core';
 import 'systemjs/dist/system-production';
 
 export interface Externals {
   readonly [externalName: string]: unknown;
 }
 
+/**
+ * @param externals An object with shared npm dependencies that the integrator
+ * wants to provide to Feature Apps. The keys are the names of the dependencies
+ * that are used in import/require statements. The values are the modules.
+ */
 export function defineExternals(externals: Externals): void {
   for (const externalName of Object.keys(externals)) {
     const external = externals[externalName];
@@ -13,6 +17,12 @@ export function defineExternals(externals: Externals): void {
   }
 }
 
-export const loadAmdModule: ModuleLoader = async (
-  url: string
-): Promise<unknown> => SystemJS.import(url);
+/**
+ * @param url A URL pointing to a bundle that was built as an AMD or UMD module.
+ *
+ * @returns A promise that resolves with the loaded module, or is rejected if
+ * the module can not be loaded.
+ */
+export async function loadAmdModule(url: string): Promise<unknown> {
+  return SystemJS.import(url);
+}
