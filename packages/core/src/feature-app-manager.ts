b03f7dd47aed13739b414cdf930986cf2f563e62
Mark Vollmann <mark.vollmann@sinnerschrader.com>
2022-01-27 10:32:37 +0100

    feat(all): improve JSDoc comment

--- packages/core/src/feature-app-manager.ts
+++ packages/core/src/feature-app-manager.ts
@@ -46,8 +46,10 @@ export interface FeatureAppEnvironment<
    * function when it has completed its task. The Integrator (or parent Feature
    * App) can then decide to e.g. unmount the Feature App.
    *
-   * By default it is assumed that the FA exited successfully, but it can signal
-   * that it failed. It is up to the integrator to handle this.
+   * Optionally, the Feature App can pass a result
+   * into the done callback. The type/structure of
+   * the result must be agreed between the Integrator
+   * (or parent Feature App) and the Feature App.
    */
   readonly done?: (result?: unknown) => void;
 }
@@ -122,8 +124,10 @@ export interface FeatureAppScopeOptions<
    * task. The Integrator (or parent Feature App) can then decide to e.g.
    * unmount the Feature App.
    *
-   * By default it is assumed that the FA exited successfully, but it can signal
-   * that it failed. It is up to the integrator to handle this.
+   * Optionally, the Feature App can pass a result
+   * into the done callback. The type/structure of
+   * the result must be agreed between the Integrator
+   * (or parent Feature App) and the Feature App.
    */
   readonly done?: (result?: unknown) => void;
 }
