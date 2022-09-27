e14568cf64e9a9cd9f2f07bae8d972c0c15d8ded
Hendrik Liebau <mail@hendrik-liebau.de>
2022-09-27 18:32:46 +0200

    fix(history-service): do not ignore consumer history changes on root location push (#720)

--- packages/history-service/src/internal/create-key.ts
+++ packages/history-service/src/internal/create-key.ts
@@ -1,4 +1,4 @@
 // source: https://github.com/remix-run/history/blob/485ebc1/packages/history/index.ts#L1043
 export function createKey(): string {
-  return Math.random().toString(36).substr(2, 8);
+  return Math.random().toString(36).slice(2, 8);
 }
