d5790edec5cc63cb7858a0c95111f0c24d0f2445
Hendrik Liebau <mail@hendrik-liebau.de>
2023-12-21 16:38:27 +0100

    feat(async-ssr-manager): support concurrent `renderUntilCompleted` calls
    When the integrator is not a React app, and renders multiple feature
    apps concurrently with separate `ReactDOM.renderToString` calls, each
    wrapped with `renderUntilCompleted`, we must scope the scheduled async
    operations to the respective `renderUntilCompleted` call. This is
    accomplished by leveraging `AsyncLocalStorage`.
    
    Note: This scenario did kind of work already, by accident, up until
    v2.9.0. But it was broken with #702 which we shipped with
    [v2.10.0](https://github.com/sinnerschrader/feature-hub/releases/tag/v2.10.0).

--- packages/async-ssr-manager/src/internal/async-ssr-manager.ts
+++ packages/async-ssr-manager/src/internal/async-ssr-manager.ts
@@ -1,3 +1,4 @@
+import {AsyncLocalStorage} from 'async_hooks';
 import {AsyncSsrManagerV1} from '..';
 import {AsyncSsrManagerContext} from './async-ssr-manager-context';
 import {setTimeoutAsync} from './set-timeout-async';
@@ -9,7 +10,9 @@ async function renderingTimeout(timeout: number): Promise<never> {
 }
 
 export class AsyncSsrManager implements AsyncSsrManagerV1 {
-  private readonly asyncOperations = new Set<Promise<unknown>>();
+  private readonly asyncOperationsStorage = new AsyncLocalStorage<
+    Set<Promise<unknown>>
+  >();
 
   public constructor(
     private readonly context: AsyncSsrManagerContext,
@@ -19,41 +22,52 @@ export class AsyncSsrManager implements AsyncSsrManagerV1 {
   public async renderUntilCompleted(
     render: () => Promise<string> | string,
   ): Promise<string> {
-    const renderPromise = this.renderingLoop(render);
+    const asyncOperations = new Set<Promise<unknown>>();
 
-    if (typeof this.timeout !== 'number') {
-      this.context.logger.warn(
-        'No timeout is configured for the Async SSR Manager. This could lead to unexpectedly long render times or, in the worst case, never resolving render calls!',
-      );
+    return this.asyncOperationsStorage.run(asyncOperations, async () => {
+      const renderPromise = this.renderingLoop(render, asyncOperations);
 
-      return renderPromise;
-    }
+      if (typeof this.timeout !== 'number') {
+        this.context.logger.warn(
+          'No timeout is configured for the Async SSR Manager. This could lead to unexpectedly long render times or, in the worst case, never resolving render calls!',
+        );
 
-    return Promise.race([renderPromise, renderingTimeout(this.timeout)]);
+        return renderPromise;
+      }
+
+      return Promise.race([renderPromise, renderingTimeout(this.timeout)]);
+    });
   }
 
   public scheduleRerender(
     asyncOperation: Promise<unknown> = Promise.resolve(),
   ): void {
-    this.asyncOperations.add(asyncOperation);
+    const asyncOperations = this.asyncOperationsStorage.getStore();
+
+    if (!asyncOperations) {
+      throw new Error(
+        'Async SSR Manager: Can not call `scheduleRerender` outside of `renderUntilCompleted`.',
+      );
+    }
+
+    asyncOperations.add(asyncOperation);
   }
 
   private async renderingLoop(
     render: () => Promise<string> | string,
+    asyncOperations: Set<Promise<unknown>>,
   ): Promise<string> {
     let html = await render();
 
-    while (this.asyncOperations.size > 0) {
-      while (this.asyncOperations.size > 0) {
+    while (asyncOperations.size > 0) {
+      while (asyncOperations.size > 0) {
         // Storing a snapshot of the asynchronous operations and clearing them
         // afterwards, allows that consecutive promises can be added while the
         // current asynchronous operations are running.
 
-        const asyncOperationsSnapshot = Array.from(
-          this.asyncOperations.values(),
-        );
+        const asyncOperationsSnapshot = Array.from(asyncOperations.values());
 
-        this.asyncOperations.clear();
+        asyncOperations.clear();
 
         await Promise.all(asyncOperationsSnapshot);
       }
