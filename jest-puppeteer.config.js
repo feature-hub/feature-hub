107c8dd63c08a80a310b55f4a3407ee459674470
Stefan Meyer <s.meyer@accenture.com>
2026-06-22 15:11:52 +0200

    build(all): add lock file cache to workflow

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -2,17 +2,11 @@
 
 const fs = require('fs');
 
-const macChromePaths = [
-  '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
-  '/Applications/Chromium.app/Contents/MacOS/Chromium',
-];
 
-const executablePath = macChromePaths.find((path) => fs.existsSync(path));
 
 /** @type {import('jest-environment-puppeteer').JestPuppeteerConfig} */
 module.exports = {
   launch: {
-    ...(executablePath ? {executablePath} : {}),
     headless: 'new',
     args: ['--no-zygote', '--no-sandbox'],
   },
