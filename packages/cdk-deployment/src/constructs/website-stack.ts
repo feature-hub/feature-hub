1cf233d18bf6d8cf4cd57c9f363c20a3d1e985f6
Robert Rathsack <robert.rathsack@sinnerschrader.com>
2026-09-23 15:01:53 +0200

    fix(cdk-deployment): add missing CSP hashes

--- packages/cdk-deployment/src/constructs/website-stack.ts
+++ packages/cdk-deployment/src/constructs/website-stack.ts
@@ -113,13 +113,36 @@ export class WebsiteStack extends cdk.Stack {
       this.createCspEntry(
         'script-src-elem',
         'self',
-        'sha256-yjJ797QQq/UCxCa++V+5HS+UIQopKNOYVrgkv/NZ0B8=',
         'sha256-bI2b8zL8P3uzmgy+aB+Lh2ZEf8GRlptjS0Gs3QKMRSM=',
+        'sha256-yjJ797QQq/UCxCa++V+5HS+UIQopKNOYVrgkv/NZ0B8=',
+        // api pages
+        'sha256-10m9WSfFWWa1qM9Z5+4hPCEFJP7DRPogeAkaBk+dSEo=',
+      ),
+      this.createCspEntry(
+        'style-src',
+        'self',
+        'unsafe-hashes',
+        // api pages
+        'sha256-47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU=',
+        'sha256-9C8E9lTl9Ufzx1ixGISzNmistLBAhsnFRHORD+qYhgw=',
+        'sha256-ANwXa+n9mfOCGDPXssFD7zGY2Cs5m/i+vbEdHQ72Rbo=',
+        'sha256-OY6beE2xYjf/xQc1K2/CzTJL3bzKquql5dkXG5HnxPQ=',
       ),
       this.createCspEntry(
         'style-src-attr',
         'unsafe-hashes',
+        // default website
+        'sha256-ABQhd/Zh/+VisTFZNhQsYXuyA6IQqAWJfkHgknQG1jo=',
+        'sha256-aUTJP2L1OsdTcaUNqFKnHSLx2nYD7ng6wCSID4HUP5w=',
         'sha256-biLFinpqYMtWHmXfkA1BPeCY0/fNt46SAZ+BBk5YUog=',
+        'sha256-iamlC7IYM1qQboC8UNCKoXh7F4NsLoIZm24lvXzKM54=',
+        'sha256-JH9PGTz9RMFJX+rlfoxCXWDyhGqucRbQrwVKT0SWzAI=',
+        'sha256-LG+rzMsUvyfv5Gxnaf7y6/Q4NaCaegFB5aqBq2OYb6Q=',
+        'sha256-p+aiqswXihF3tvngLMNP8d/S7iGc3+CRXDSV6HBjTME=',
+        'sha256-R6+bxKX1OG1hXBjea33/YlIGKXed2uAQrlUJnx5O6tI=',
+        'sha256-wHbQxigg9aQsPujn/l4xOgX5SxAppag0RmED0WYNp60=',
+        'sha256-X4NQZKRjLUxIwTx3QUKH35Nay23gJGsXjtq4Idy+cKE=',
+        // api pages
       ),
     ].join(' ');
 
